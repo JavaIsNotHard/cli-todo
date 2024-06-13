09c81968deb0ae93491f80d36e88442e0f8291d2
JavaIsNotHard <jibeshshrestha00@gmail.com>
2024-06-13 18:20:18 +0545

    add database support

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
+	"chat/internal/data"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+func openDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./users.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
 func main() {
+	db := openDB()
+	defer db.Close()
+
+	var todoModel data.NewModel(db)
+
 	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 
 	if _, err := p.Run(); err != nil {
